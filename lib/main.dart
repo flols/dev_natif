8150e235ee3b4b53b1393115df5ef0395f1e14a4
FlorianLS <fflols22@gmail.com>
2023-11-29 14:37:26 +0100

    create a GridView widget for resources

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,68 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ressources'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: 4, // 4 types de ressources
+        itemBuilder: (BuildContext context, int index) {
+          return Ressource(index: index);
+        },
+      ),
+    );
+  }
+}
+
+class Ressource extends StatelessWidget {
+  final int index;
+
+  const Ressource({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Color> colors = [
+      const Color(0xFF967969), // Bois
+      const Color(0xFFCED4DA), // Minerai de fer
+      const Color(0xFFD9480F), // Minerai de cuivre
+      const Color(0xFF000000), // Charbon
+    ];
+
+    List<String> descriptions = [
+      'Bois brut',
+      'Minerai de fer brut',
+      'Minerai de cuivre brut',
+      'Minerai de charbon',
+    ];
+
+    Color color = colors[index];
+
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          descriptions[index],
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
         ),
       ),
     );
